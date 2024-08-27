8cd553b8ef7677cba9d5ed2aed1c5907161fdf7c
Gihan Ediriweera Jayakuru <gihan22603@gmail.com>
2024-08-27 22:22:31 +0530

    error exception

--- src/messages/messsage.controller.ts
+++ src/messages/messsage.controller.ts
@@ -1,4 +1,11 @@
-import { Body, Controller, Get, Param, Post } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Get,
+  NotFoundException,
+  Param,
+  Post,
+} from '@nestjs/common';
 import { CreateMessageDto } from './dtos/create-message.dto';
 import { MessagesService } from './messages.service';
 
@@ -23,8 +30,14 @@ export class MesssageController {
   }
 
   @Get('/:id')
-  getMessage(@Param('id') id: string) {
+  async getMessage(@Param('id') id: string) {
     // console.log(id);
-    return this.messagesService.findOne(id);
+    const message = await this.messagesService.findOne(id);
+
+    if (!message) {
+      throw new NotFoundException('message not found');
+    }
+
+    return message;
   }
 }
