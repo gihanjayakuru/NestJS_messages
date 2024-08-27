ce566a12e44b2c7b97650f028d8ad9d7dcada079
Gihan Ediriweera Jayakuru <gihan22603@gmail.com>
2024-08-27 22:16:49 +0530

    test controller

--- src/messages/messages.repository.ts
+++ src/messages/messages.repository.ts
@@ -11,6 +11,8 @@ export class messagesRepository {
   async findAll() {
     const contents = await readFile('messages.json', 'utf8');
     const messages = JSON.parse(contents);
+
+    return messages;
   }
 
   async create(content: string) {
--- src/messages/messsage.controller.ts
+++ src/messages/messsage.controller.ts
@@ -1,18 +1,30 @@
 import { Body, Controller, Get, Param, Post } from '@nestjs/common';
 import { CreateMessageDto } from './dtos/create-message.dto';
+import { MessagesService } from './messages.service';
 
 @Controller('messages')
 export class MesssageController {
+  messagesService: MessagesService;
+
+  constructor() {
+    //for test later use dep injection
+    this.messagesService = new MessagesService();
+  }
+
   @Get()
-  listMessages() {}
+  listMessages() {
+    return this.messagesService.findAll();
+  }
 
   @Post()
   createMessages(@Body() body: CreateMessageDto) {
-    console.log(body);
+    // console.log(body);
+    return this.messagesService.create(body.content);
   }
 
   @Get('/:id')
   getMessage(@Param('id') id: string) {
-    console.log(id);
+    // console.log(id);
+    return this.messagesService.findOne(id);
   }
 }
