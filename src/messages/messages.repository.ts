92b5be5454af63a412ba2f8bc51c24c2b79d57ec
Gihan Ediriweera Jayakuru <gihan22603@gmail.com>
2024-08-27 21:58:30 +0530

    reading and write file

--- src/messages/messages.repository.ts
+++ src/messages/messages.repository.ts
@@ -1,4 +1,4 @@
-import { readFile } from 'fs/promises';
+import { readFile, writeFile } from 'fs/promises';
 
 export class messagesRepository {
   async findOne(id: string) {
@@ -8,7 +8,19 @@ export class messagesRepository {
     return messages[id];
   }
 
-  async findAll() {}
+  async findAll() {
+    const contents = await readFile('messages.json', 'utf8');
+    const messages = JSON.parse(contents);
+  }
 
-  async create(message: string) {}
+  async create(content: string) {
+    const contents = await readFile('messages.json', 'utf8');
+    const messages = JSON.parse(contents);
+
+    const id = Math.floor(Math.random() * 999);
+
+    messages[id] = { id, content };
+
+    await writeFile('messages.json', JSON.stringify(messages));
+  }
 }
